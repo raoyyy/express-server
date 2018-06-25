be3c316e15187fd5024bf2f65305a4f603104319
Raoyu <q360584748@qq.com>
2018-06-25 14:53:27 +0800

    完成错题功能

--- utils/dbActions.js
+++ utils/dbActions.js
@@ -31,78 +31,109 @@ var getQuestions = function(){
     })
 };
 
-var getMistakes = function(){
+var getMyUserInfo = function(user_id){
     return new Promise(function(resolve,reject){
         //暂时不做权限限制，只要知道 user_id 就可以获取用户数据
-        users.find({"_id":user_id},{},async function (e,doc){
-            if (e) {
-                res.send("查找数据时发生错误");
-            } 
-            else {
-                if (doc.length == 0){
-                    reject(new Error("couldn't find userInfo"));
-                }
-                else {
-                    var userInfo = doc[0];
-                    var note = await getNote(openid,user_id)
-                    // 需要知道请求者的 openid 才能返回对应的备注名
-                    if (note.noteName){
-                        userInfo.info.noteName = note.noteName
+        users.find({"user_id":user_id},{},async function (e,doc){
+            // if (e) {
+            //     res.send("查找数据时发生错误");
+            // } 
+            // else {
+                 if (doc.length == 0){
+                    var newUser = {
+                        "user_id":"raoyu0928",
+                        "openid": "openid1",
+                        "department":"前端开发",
+                        "avatarUrl":"",
+                        "name":"饶煜。",
+                        "records":[],
+                        "mistakes":[]
                     }
-                    resolve(userInfo);
+                    users.insert(newUser, function(e,doc){
+                        if(e){
+                            reject(new Error("在 users 表中插入新用户数据时发生错误"))
+                        }
+                        else {
+                            resolve(newUser)
+                        }
+                    })
                 }
-            }
-        })
-    })
-};
-
-var updateMistakes = function(user_id){
-    return new Promise(async (resolve,reject) => {
-        user_id.map(user_id => {
-            questions.find({"_id":user_id},{},function (e,doc){
-                if (e) {
-                    reject(new Error("查找user表数据时发生错误"));
-                } 
                 else {
-                    // if (doc.length == 0){
-                    //     var newUser = {
-                    //         "user_id":user_id,
-                    //         // "avatarUrl":"",
-                    //         // "records":[],
-                    //         "mistakes":{}
-                    //     }
-                        // users.insert(newUser,function (e,doc){
-                        //     if (e){
-                        //         reject(new Error("在 users 表中插入新用户数据时发生错误"))
-                        //     }
-                        //     else {
-                        //         resolve(newUser);
-                        //     }
+                    var myUserInfo = doc[0];
+                    resolve(myUserInfo)
+                    // var myUserInfo = doc[0];
+                    // var mistakes = myUserInfo.mistakes;//成功返回错题id
+                    // mistakes.map(mistake => {
+                    //     questions.find({"_id":mistake},{},async function (e,doc){
+
                     //     })
-                    // }
-                    // else{
-                        var mistakes = doc[0];
-                        users.insert(mistakes,function (e,doc){
-                            if (e){
-                                reject(new Error("在 users 表中插入新用户数据时发生错误"))
-                            }
-                            else {
-                                resolve(mistakes);
-                            }
-                        })                       
-                        resolve(mistakes);
-                    
+                    // })
+                    // resolve(myUserInfo)
+                }
+            
+         })
+        })
+    }
+
+var updateMyUserInfo = function(user_id,myUserInfo) {
+        //这里更新的对象也应该是完整的 用户表数据 userInfo
+        return new Promise(function(resolve, reject){
+            users.update({"user_id":user_id},myUserInfo, function (e, doc) {
+                if (e) {
+                    reject(new Error("这里更新数据时发生错误"));
+                }
+                else {
+                    resolve(doc);
                 }
             })
         })
+    };
+
+var getMistakes = function(user_id){
+    return new Promise(async (resolve,reject) => {      
+        questions.find({"_id":user_id},{},function (e,doc){
+            if (e) {
+                reject(new Error("查找user表数据时发生错误"));
+            } 
+            else {
+                var mistakes = doc[0]
+                resolve(mistakes)
+            }
+        })
     })
+}
+
+// var updateUsers = function(user_id,userInfo){
+//     return new Promise(async (resolve,reject) => {
+//         user_id.map(user_id => {
+//             questions.find({"_id":user_id},{},function (e,doc){
+                // if (e) {
+                //     reject(new Error("查找user表数据时发生错误"));
+                // } 
+                // else {
+                //         var mistakes = [];
+                //         mistakes.push(user_id)                  
+                // }
+//             })
+//         })
+//         users.insert(mistakes,function (e,doc){
+//             if (e){
+//                 reject(new Error("在 users 表中插入新用户数据时发生错误"))
+//             }
+//             else {
+//                 resolve(mistakes);
+//             }
+//         })                       
+//         resolve(mistakes);
+//     })
     
-};
+// };
 
 
 module.exports = {
     addQuestion,
     getQuestions,
-    getMistakes,
-    updateMistakes
+    getMyUserInfo,
+    updateMyUserInfo,
+    getMistakes
 }
